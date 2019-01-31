a69c8d5faa2295ea987b974d34446432f1ae5c80
Juliiia <jk.hohnstein@hotmail.com>
2019-01-31 14:49:52 +0100

    clean project

--- src/static/backend/NetworkGraph.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3.6
-class NetworkGraph:
-    def __init__(self, file):
-        self.file = file
-
-    def testoutput(self):
-        fp = open("networkgraph.txt", "w")
-        fp.write(self.file)
-        return "network graph"
--- src/static/backend/OutPutPrinter.py
+++
@@ -1,7 +0,0 @@
-class OutPutPrinter:
-    def __init__(self, file):
-        self.file = file
-
-    def printOut(self):
-        fp = open("log/logging.txt", "w")
-        fp.write(self.file)
