4a847786155b61be43ccee10fdbb5ee659b0d287
Juliiia <jk.hohnstein@hotmail.com>
2019-02-22 19:12:21 +0100

    save 2

--- src/static/js/visualizations/NetworkgraphCreator.js
+++ src/static/js/visualizations/NetworkgraphCreator.js
@@ -25,14 +25,13 @@ class NetworkgraphCreator{
         // create parent svg and title
         let childDiv = document.createElement('div');
         childDiv.setAttribute('id', 'svgDiv' + artifactName);
-        childDiv.setAttribute('class', 'halfWidth');
 
         let childId = UiElementLib.getSVGId(artifactName);
 
         // clean old
         UiElementLib.removeChildElementIfExists(this.parentElement, '#' + childId);
 
-        let svgElement = UiElementLib.getSVGTag(childId, 900, 800);
+        let svgElement = UiElementLib.getSVGTag(childId, 1000, 800);
 
         // create group for each artifact json
         let group = document.createElementNS("http://www.w3.org/2000/svg", "g");
@@ -131,7 +130,7 @@ class NetworkgraphCreator{
     highlightNodeAndLinks(element){
         this.parentElement.find('.clicked').removeClass('clicked');
         // mark node
-        let nodeId = UiElementLib.getGlobelEntityId(element.data('artifact'), element.data('id'));
+        let nodeId = UiElementLib.getGlobalEntityFilterId(element.data('artifact'), element.data('id'), element.data('type'));
         this.parentElement.find('.' + nodeId).addClass('clicked');
         // mark dependent links
         let sourceNodeId = UiElementLib.getGlobalLinkSourceEntityId(element.data('id'));
